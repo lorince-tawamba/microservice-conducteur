6f31c5027e6b4feb23b4add0fec16cc39b62dc1f
lorincet <lorincet@gmail.com>
2021-11-27 10:37:18 +0100

    Create project

---
+++ src/main/java/tech/satic/agriapp/MicroserviceConducteurApplication.java
@@ -0,0 +1,13 @@
+package tech.satic.agriapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConducteurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConducteurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/satic/agriapp/MicroserviceConducteurApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.agriapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConducteurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
