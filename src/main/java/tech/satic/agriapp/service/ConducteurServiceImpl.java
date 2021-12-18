54da86fcb997da3683749190a5c2515e9b41df20
lorincet <lorincet@gmail.com>
2021-12-18 11:07:09 +0100

    Create

--- src/main/java/tech/satic/agriapp/service/ConducteurServiceImpl.java
+++ src/main/java/tech/satic/agriapp/service/ConducteurServiceImpl.java
@@ -1,14 +1,23 @@
 package tech.satic.agriapp.service;
 
 import org.springframework.stereotype.Service;
+import tech.satic.agriapp.dto.ConducteurRequestDTO;
+import tech.satic.agriapp.dto.ConducteurResponseDTO;
+import tech.satic.agriapp.entity.Conducteur;
+import tech.satic.agriapp.mappers.ConducteurMapper;
 import tech.satic.agriapp.repository.ConducteurRepository;
 
 @Service
 public class ConducteurServiceImpl implements ConducteurService {
 
     private ConducteurRepository conducteurRepository;
+    private ConducteurMapper conducteurMapper;
 
     public ConducteurServiceImpl(ConducteurRepository conducteurRepository) {
         this.conducteurRepository = conducteurRepository;
+    }
+
+    public ConducteurResponseDTO addConduteur(ConducteurRequestDTO conducteurRequestDTO) {
+        Conducteur conducteur = conducteurMapper.conducteurRequestDTOConducteur(conducteurRequestDTO);
     }
 }
