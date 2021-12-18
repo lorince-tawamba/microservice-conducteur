6ebe70f93e5f3602d6d5cfbbb68bdf576a136ca5
lorincet <lorincet@gmail.com>
2021-12-18 11:34:54 +0100

    Update

--- src/main/java/tech/satic/agriapp/controller/ConducteurController.java
+++ src/main/java/tech/satic/agriapp/controller/ConducteurController.java
@@ -1,9 +1,26 @@
 package tech.satic.agriapp.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.satic.agriapp.dto.ConducteurRequestDTO;
+import tech.satic.agriapp.dto.ConducteurResponseDTO;
+import tech.satic.agriapp.service.ConducteurService;
+import tech.satic.agriapp.service.ConducteurServiceImpl;
 
 @RestController
 @RequestMapping(path = "/conducteurs")
 public class ConducteurController {
+
+    private ConducteurServiceImpl conducteurServiceImpl;
+
+    public ConducteurController(ConducteurServiceImpl conducteurServiceImpl) {
+        this.conducteurServiceImpl = conducteurServiceImpl;
+    }
+
+     @PostMapping(path = "/add")
+    public ConducteurResponseDTO addConducteur(@RequestBody ConducteurRequestDTO conducteurRequestDTO) {
+        return conducteurServiceImpl.addConduteur(conducteurRequestDTO);
+    }
 }
--- src/main/java/tech/satic/agriapp/service/ConducteurService.java
+++ src/main/java/tech/satic/agriapp/service/ConducteurService.java
@@ -1,4 +1,9 @@
 package tech.satic.agriapp.service;
 
+import tech.satic.agriapp.dto.ConducteurRequestDTO;
+import tech.satic.agriapp.dto.ConducteurResponseDTO;
+
 public interface ConducteurService {
+
+    public ConducteurResponseDTO addConduteur(ConducteurRequestDTO conducteurRequestDTO);
 }
