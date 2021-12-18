54ed663e9f2da1e206ee4a2798e302ea510d5ede
lorincet <lorincet@gmail.com>
2021-12-18 10:44:59 +0100

    Create

--- src/main/java/tech/satic/agriapp/service/ConducteurServiceImpl.java
+++ src/main/java/tech/satic/agriapp/service/ConducteurServiceImpl.java
@@ -1,4 +1,14 @@
 package tech.satic.agriapp.service;
 
+import org.springframework.stereotype.Service;
+import tech.satic.agriapp.repository.ConducteurRepository;
+
+@Service
 public class ConducteurServiceImpl implements ConducteurService {
+
+    private ConducteurRepository conducteurRepository;
+
+    public ConducteurServiceImpl(ConducteurRepository conducteurRepository) {
+        this.conducteurRepository = conducteurRepository;
+    }
 }
